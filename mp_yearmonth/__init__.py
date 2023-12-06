e3e5b8b0d82644f3049c9adce443c9c3e374ce90
Ramon Torres <raymondjavaxx@gmail.com>
2023-12-06 13:27:55 -0500

    Add publish workflow (#1)
    * Add publish workflow
    
    * Add release stage
    
    * Fix
    
    * Add pypi stage
    
    * Remove __version__
    
    * Cleanup
    
    * Remove TODOs
    
    * Cleanup

--- mp_yearmonth/__init__.py
+++ mp_yearmonth/__init__.py
@@ -1,9 +1,7 @@
-# yearmonth.py
+# __init__.py
 # Copyright (c) 2023 Ramon Torres
 #
 # This module is part of mp-yearmonth and is released under
 # the MIT License: https://opensource.org/license/mit/
 
 from .yearmonth import YearMonth
-
-__version__ = "0.1.0"
